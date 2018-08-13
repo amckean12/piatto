05cbc7dba8200e0157aec70da8e2d4fc4b8b188a
amckean12 <amckean@me.com>
2018-08-12 19:46:24 -0500

    Added friendships controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'friendships/create'
+  get 'friendships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #Root handles user login or will redirect to create a new user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_160239) do
+ActiveRecord::Schema.define(version: 2018_08_13_004220) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
