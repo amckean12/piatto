75a14fb9b1b0984aa3e9cce5f67cd7c9105ca797
amckean12 <amckean@me.com>
2018-08-14 13:45:53 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   # <-End Associations->
 
   # <-Start Validations->
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
   # <- End Validations ->
 
   # <- Start Security ->
