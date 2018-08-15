7a90dae0c448b6021071657adb2731860f8b39eb
amckean12 <amckean@me.com>
2018-08-15 08:58:29 -0500

    added recipe assoc and validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  # <- Start Associations->
+  belongs_to :user
+  # <-End Associations->
+
+  # <-Start Validations->
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :calories, presence: true
+  validates :carbs, presence: true
+  validates :protein, presence: true
+  validates :fats, presence: true
+  validates :user, presence: true
+  # <- End Validations ->
+
+  # <- Start Security ->
+
+  # <- End Security ->
+
+  # <- Start Methods ->
+  # <- End Methods ->
 end
