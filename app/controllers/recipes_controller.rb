4eab84677def85cfe37849137c8804bf2b326e5a
amckean12 <amckean@me.com>
2018-08-15 09:09:59 -0500

    Added index func

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,15 @@
 class RecipesController < ApplicationController
+  before_action :require_login
+  before_action :set_user
+
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :profiles
+  resources :recipes
 end
