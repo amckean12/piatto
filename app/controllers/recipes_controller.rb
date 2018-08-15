755953d6aee07abe023cee3daa2a1f7ae2447742
amckean12 <amckean@me.com>
2018-08-15 09:44:07 -0500

    Create recipe function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,44 @@
 class RecipesController < ApplicationController
   before_action :require_login
   before_action :set_user
+  layout 'profile-layout'
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
 
   def index
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path, notice: "Deleted Recipe: #{recipe.name}"
+  end
+
+
   private
 
   def set_user
     @user = current_user
-  end 
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :calories, :carbs, :protein, :fats)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 
 User.create(first_name: 'John', last_name: "Smith", email: "jsmith@me.com", password: "test")
 Profile.create(user_id: 1)
+
+Recipe.create(name: 'Street Tacos', description: 'Tortilla-Chicken-Pico-Cheese-Avacado', calories: 230, carbs: 75, protein: 45, fats: 18, user_id: 2)
+Recipe.create(name: 'Bar Burger', description: 'Bun-Beef-Tomato-Lettuce-Onion-Cheese-Bacon', calories: 400, carbs: 100, protein: 60, fats: 30, user_id: 2)
+Recipe.create(name: 'Pepperoni Pizza', description: 'Crust-Tomato Sauce-Cheese-Pepperoni', calories: 300, carbs: 75, protein: 10, fats: 18, user_id: 2)
+Recipe.create(name: 'Ribs', description: 'Ribs-BBQ Sauce', calories: 500, carbs: 0, protein: 70, fats: 18, user_id: 2)
+Recipe.create(name: 'Omellete', description: 'Eggs-Cheese-Peppers-Bacon', calories: 400, carbs: 20, protein: 45, fats: 30, user_id: 2)
+Recipe.create(name: 'Burrito', description: 'Tortilla-Chicken-Pico-Cheese-Avacado-Sour Cream', calories: 230, carbs: 75, protein: 45, fats: 18, user_id: 2)
+Recipe.create(name: 'Ice Cream', description: 'Ice Cream', calories: 600, carbs: 75, protein: 45, fats: 18, user_id: 2)
