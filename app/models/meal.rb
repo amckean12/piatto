f95b0ada3172f8b85c3f247bd5ef33d6acbbc52c
amckean12 <amckean@me.com>
2018-08-16 11:12:24 -0500

    Gen menu controleer and routes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,20 @@
 class Meal < ApplicationRecord
+  # <- Start Associations->
+  belongs_to :recipe
+  belongs_to :menu
+  # <-End Associations->
+
+  # <-Start Validations->
+  validates :date, presence: true
+  validates :menu, presence: true
+  validates :recipe, presence: true
+  # <- End Validations ->
+
+  # <- Start Security ->
+
+  # <- End Security ->
+
+  # <- Start Methods ->
+
+  # <- End Methods ->
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,27 @@
 class Menu < ApplicationRecord
+  # <- Start Associations->
+  belongs_to :user
+  # <-End Associations->
+
+  # <-Start Validations->
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user, presence: true
+  # <- End Validations ->
+
+  # <- Start Security ->
+
+  # <- End Security ->
+
+  # <- Start Methods ->
+  def build_menu
+    recipe_ids = user.recipes.pluck(:id)
+    (start_date..end_date).each do |date|
+      unused_recipes = recipe_ids - meals.map(&:recipe_id)
+      available_recipes = unused_recipes.empty? ? recipe_ids : unused_recipes
+      meals.build(date: date, recipe_id: available_recipes.sample)
+    end
+  end
+
+  # <- End Methods ->
 end
