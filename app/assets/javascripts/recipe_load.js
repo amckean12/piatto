195c7888386d3c6d7917a59dc71fd32e4c6eadc0
amckean12 <amckean@me.com>
2019-02-06 19:13:17 -0600

    Added loadRecipeContent()

--- app/assets/javascripts/recipe_load.js
+++ app/assets/javascripts/recipe_load.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
 });
 
 function attachListeners(){
+  //When this button is clicked run the loadRecipes() function.
   $(".recipe-link").on("click", function(){
     loadRecipes();
+  });
+
+  //This is a way to add an event listener to a button that hasnt been created yet
+
+  $(".profile-content").on('click', ".listed-recipe-link", function(){
+    loadRecipeContent();
   });
 }
 
@@ -13,7 +20,11 @@ function loadRecipes(){
   let id = $(this).data("id");
   $.get(`/users/id/recipes.json`, function(recipe){
     $.each(recipe, function(key, value){
-      alert(key + ": " + value.name);
+      $(".profile-content").append(`<button class="listed-recipe-link">${value.name}</button><br><br>`).data(value.id);
     });
   });
+}
+
+function loadRecipeContent(){
+  alert("shes an easy loverrr");
 }
