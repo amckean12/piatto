7c1c1a4b40fdbcd8153aea2c504f3f0c74a5cd18
amckean12 <amckean@me.com>
2019-02-15 12:29:55 -0600

    Added recipe show page dynamics

--- app/assets/javascripts/recipe_load.js
+++ app/assets/javascripts/recipe_load.js
@@ -13,15 +13,21 @@ function attachListeners(){
     let data = $.parseJSON($(this).attr('data-button'));
     loadRecipeContent(data);
   });
+
+  $(".profile-content").on('click', ".visit-recipe-link", function(){
+    let data = $.parseJSON($(this).attr('data-button'));
+    createRecipePage(data);
+  });
 }
 
 
 function loadRecipes(){
   //pulling from api
   $.get(`/users/id/recipes.json`, function(recipe){
+    $(".profile-content").append("<div class=loaded-recipes></div>")
     $.each(recipe, function(key, value){
       //creating recipe containers for each recipe
-      $(".profile-content").append(`<div class="recipe-${value.id}-container"><button class="listed-recipe-link" data-button="${value.id}">${value.name}</button></div>`);
+      $(".loaded-recipes").append(`<div class="recipe-${value.id}-container"><button class="listed-recipe-link" data-button="${value.id}">${value.name}</button></div>`);
     });
   });
 }
@@ -40,7 +46,31 @@ function loadRecipeContent(recipe_id){
         <li>Carbs: ${recipe.carbs}</li>
         <li>Protein: ${recipe.protein}</li>
         <li>Fats: ${recipe.fats}</li>
+        <div class="visit-recipe-${recipe.id}-container"><button class="visit-recipe-link" data-button="${recipe.id}">Visit Recipe Page</button></div>
       </ul>
     </div>`);
+  })
+}
+
+//Need to add creator 
+function createRecipePage(recipe_id){
+  let recipeID = recipe_id;
+  $(".loaded-recipes").remove();
+  $.get(`/users/id/recipes/${recipeID}.json`, function(recipe){
+    $(".profile-content").append(`<div class="recipe-${recipe.id}-container">
+    <h1>${recipe.name}</h1>
+    <h2>Description:</h2>
+    <p>${recipe.description}</p>
+    <h2>Calories:</h2>
+    <p>${recipe.calories}</p>
+    <h2>Carbs:</h2>
+    <p>${recipe.carbs}</p>
+    <h2>Protein:</h2>
+    <p>${recipe.protein}</p>
+    <h2>Fats:</h2>
+    <p>${recipe.fats}</p>
+    <h2>Created By:</h2>
+    <p>${recipe.creator}</p>
+    </div>`)
   })
 }
