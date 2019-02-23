0e3bb01f57c87eee55e6eeccbfcaf493edc96484
amckean12 <amckean@me.com>
2019-02-23 10:53:56 -0600

    Created empty display data function

--- app/assets/javascripts/menu_load.js
+++ app/assets/javascripts/menu_load.js
@@ -14,6 +14,23 @@ function loadMenu(){
     method: 'get',
     dataType: 'json'
   }).done(function(data){
-    console.log("this is my data:", data)
+    //setting a variable that will create a JS Model Object of the most recent menu created by the user
+    let currentUserMenu = new Menu(data[data.length-1])
+    displayMenu(currentUserMenu);
   })
+}
+
+class Menu {
+  constructor(obj){
+    this.id = obj.id
+    this.start_date = obj.start_date
+    this.end_date = obj.end_date
+    this.meals = obj.meals
+  }
+}
+
+function displayMenu(data){
+  let currentUserMenu = data;
+  debugger;
+  $(".profile-content").append(`div class="menu"`)
 }
